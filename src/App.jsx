724f4b2e35e684193cb2f4931783625a67938dcd
ibra2407 <mehloldex@gmail.com>
2023-10-30 11:07:20 +0800

    if token has been used and user chooses to visit the site again with same token, will prevent any game by checking localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,23 @@ function App() {
     // Unique ID parameter is not present in the URL
     return (
       <div className="App">
-        <h1 className="centered-heading">Here for GivingHeroes2024? <br></br> <br></br> Please get a unique ID from SKH DO!</h1>
+        <h1 className="centered-heading"> Here for GivingHeroes 2024? <br></br> <br></br> Please get a unique ID from SKH DO!</h1>
       </div>
     );
   }
 
+  const isDonated = localStorage.getItem(`UniqueID ${uniqueID}`);
+
+  if (isDonated) {
+    // Unique ID is present in local storage, indicating donation has been made
+    return (
+      <div className="App">
+        <h1 className="centered-heading"> This token has been used! <br></br> Thanks for donating, Hero!</h1>
+      </div>
+    );
+  }
+
+  // Unique ID is present in the URL but not in local storage, render the game
   return (
     <div className="App">
       <h1 className="centered-heading">GivingHeroes 2024</h1>
