2d5c0164f0cabaa458ddaeb9276419db7802c721
ibra2407 <mehloldex@gmail.com>
2023-10-30 10:47:21 +0800

    ensured empty id does not allow for game to be played

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,25 @@ import Game from "./components/Game";
 import "./styles.css";
 
 function App() {
+  const searchParams = new URLSearchParams(window.location.search);
+  const uniqueID = searchParams.get("id");
+
+  if (!uniqueID) {
+    // Unique ID parameter is not present in the URL
+    return (
+      <div className="App">
+        <h1 className="centered-heading">Here for GivingHeroes2024? <br></br> <br></br> Please get a unique ID from SKH DO!</h1>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <h1 className="centered-heading">GivingHeroes 2024</h1>
-      <hr>
-      </hr>
+      <hr />
       <Game />
     </div>
   );
 }
-
-// only pass in uID and fig
-// create a usestate to keep track of completion (now done on backend side; matching)
-// when completed
-// use params to pass value from parent to child
 
 export default App;
