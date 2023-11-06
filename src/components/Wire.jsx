add4388b604aaebe15d94eead97faeeb1a5b3de6
ibra2407 <mehloldex@gmail.com>
2023-11-06 17:03:38 +0800

    wire edits

--- src/components/Wire.jsx
+++ src/components/Wire.jsx
@@ -3,7 +3,7 @@ import './Wire.css';
 
 const Wire = ({ xCoordinate, isMoving }) => {
   return (
-    <div className={`wire ${isMoving ? 'extend-animation' : 'retract-animation'}`} style={{ left: `${xCoordinate * 60 + 28}px` }} />
+    <div className={`wire ${isMoving ? 'extend-animation' : 'retract-animation'}`} style={{ left: `${xCoordinate * 60 + 29}px` }} />
   );
 };
 
