444f63a169383168cd67e577eaf0936d0211aa0f
ibra2407 <mehloldex@gmail.com>
2023-11-08 11:26:04 +0800

    tut done

--- src/components/Tutorial.jsx
+++ src/components/Tutorial.jsx
@@ -7,6 +7,7 @@ import hosp4 from './gameimg/hosp4.png'
 import hosp5 from './gameimg/hosp5.png'
 import tut1 from './gameimg/tut1.png'
 import tut2 from './gameimg/tut2.png'
+import tut3 from './gameimg/tut3.png'
 
 const Tutorial = ({ onClose }) => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -46,55 +47,62 @@ const Tutorial = ({ onClose }) => {
           <div className="page-content">
             <img src={hosp1} className="tutorial-image" />
             <br></br>
-            P1
+            {/* P1 */}
           </div>
         )}
         {currentPage === 2 && (
           <div className="page-content">
             <img src={hosp2} className="tutorial-image" />
             <br></br>
-            P2
+            {/* P2 */}
           </div>
         )}
         {currentPage === 3 && (
           <div className="page-content">
             <img src={hosp3} className="tutorial-image" />
             <br></br>
-            P3
+            {/* P3 */}
           </div>
         )}
         {currentPage === 4 && (
           <div className="page-content">
             <img src={hosp4} className="tutorial-image" />
             <br></br>
-            P4
+            {/* P4 */}
           </div>
         )}
         {currentPage === 5 && (
           <div className="page-content">
             <img src={hosp5} className="tutorial-image" />
             <br></br>
-            P5
+            {/* P5 */}
           </div>
         )}
         {currentPage === 6 && (
           <div className="page-content">
             <img src={tut1} className="tutorial-image" />
             <br></br>
-            T1
+            Move the left and right arrow keys to move the claw.
           </div>
         )}
         {currentPage === 7 && (
           <div className="page-content">
             <img src={tut2} className="tutorial-image" />
             <br></br>
-            T2
+            Press the spacebar when you want to pick up a box.
+          </div>
+        )}
+        {currentPage === 8 && (
+          <div className="page-content">
+            <img src={tut3} className="tutorial-image" />
+            <br></br>
+            Find out where the mystery boxes are to retrieve your hero!
           </div>
         )}
 
         <div className="navigation-buttons">
           {currentPage > 1 && <button className="prev-button" onClick={handlePrevPage}>Prev</button>}
-          {currentPage < 7 && <button className="next-button" onClick={handleNextPage}>Next</button>}
+          {currentPage < 8 && <button className="next-button" onClick={handleNextPage}>Next</button>}
         </div>
         <button className="close-button" onClick={handleClose}>Close</button>
       </div>
