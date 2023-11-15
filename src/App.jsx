88cd9941df8fb5475dae96f051ade1a711d10709
ibra2407 <mehloldex@gmail.com>
2023-11-15 16:13:45 +0800

    freeze screens settled

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,17 @@ function App() {
   const searchParams = new URLSearchParams(window.location.search);
   const uniqueID = searchParams.get('id');
   const [showTutorial, setShowTutorial] = useState(false);
+  const [enableSparkle, setEnableSparkle] = useState(true);
 
   if (!uniqueID) {
     // Unique ID parameter is not present in the URL
     return (
-      <div className="App">
-        <h1 className="centered-heading large-text">
-          Here for SKH GivingHeroes 2024?<br></br> Please get a unique ID from SKH DO!
-        </h1>
+      <div className="overlay">
+        <div>
+          <h1 className="centered-heading large-text">
+            Here for SKH GivingHeroes 2024?<br></br> Please get a unique ID from SKH DO!
+          </h1>
+        </div>
       </div>
     );
   }
@@ -25,13 +28,17 @@ function App() {
   if (isDonated) {
     // Unique ID is present in local storage, indicating donation has been made
     return (
-      <div className="App">
-        <h1 className="centered-heading large-text">
-          This token has been used! <br></br> Thanks for donating, Hero!
-        </h1>
+      <div className="overlay">
+        <div>
+          <h1 className="centered-heading large-text">
+            This token has been used! <br></br> Thanks for donating, Hero!
+          </h1>
+        </div>
       </div>
     );
   }
+
+  
 
   // Unique ID is present in the URL but not in local storage, render the game and tutorial button
   return (
