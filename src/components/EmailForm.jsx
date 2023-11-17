1403b5950c0ede980c2140fc902a4d23c4e5a723
ibra2407 <mehloldex@gmail.com>
2023-11-17 17:24:50 +0800

    idk if this email edit will work

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -1,57 +1,46 @@
-import React from 'react'
-import emailjs from '@emailjs/browser';
+import React from 'react';
+import smtp from 'smtp-js';
 
 const EmailForm = ({ UniqueID, superheroName, onEmailSent, alreadySent }) => {
-
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Your EmailJS service ID, template ID, and Public Key
-    const serviceId = 'service_arkp1em';
-    const templateId = 'template_tqp2w0f';
-    const publicKey = 'Qps1VbyFe8Ddg4MuA';
+    Email.send({
+      SecureToken : "75960234-ce73-4c16-8cbe-4df7491e3a7a",
+      To : 'muhammad_ibrahim@mymail.sutd.edu.sg',
+      From : "skh-gh-24@skh.com",
+      Subject : `Figurine Request - ${superheroName}`,
+      Body :
+      `Unique ID: ${UniqueID}
+      Figurine: ${superheroName}
+      Thank you for your donation, hero!`,
+    }).then(
+    message => alert(message)
+    );
 
-    // Create a new object that contains dynamic template params
-    const templateParams = {
-      uid: UniqueID,
-      fig: superheroName,
-    };
-    if (!alreadySent){
-      // Send the email using EmailJS
-      emailjs
-      .send(serviceId, templateId, templateParams, publicKey)
-      .then((response) => {
-        console.log('Email sent successfully!', response);
-        alert("You've completed the game! Thank you for your donation!");
-      })
-      .catch((error) => {
-        console.error('Error sending email:', error);
-      });
-    } else{
-      console.log("email has already been sent with this id!")
-      alert("Token has expired! Thank you for your donation!")
-      // just send them to a new page as failsafe
+    if (!alreadySent) {
+      smtp
+        .send(emailConfig)
+        .then(() => {
+          console.log('Email sent successfully!');
+          alert("You've completed the game! Thank you for your donation!");
+        })
+        .catch((error) => {
+          console.error('Error sending email:', error);
+        });
+    } else {
+      console.log('Email has already been sent with this id!');
+      alert('Token has expired! Thank you for your donation!');
     }
     onEmailSent();
-    
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit} className='emailForm'>
-        <input
-          type="text"
-          placeholder="Unique ID"
-          value={UniqueID}
-          disabled
-        />
-        <input
-          type="text"
-          placeholder="Figurine"
-          value={superheroName}
-          disabled // Disables field from edits
-        />
-        <button type="submit">I'll take it! Let's go!</button>
+        <input type='text' placeholder='Unique ID' value={UniqueID} disabled />
+        <input type='text' placeholder='Figurine' value={superheroName} disabled />
+        <button type='submit'>I'll take it! Let's go!</button>
       </form>
     </div>
   );
@@ -59,14 +48,18 @@ const EmailForm = ({ UniqueID, superheroName, onEmailSent, alreadySent }) => {
 
 export default EmailForm;
 
-// Email.send({
-//     Host : "smtp.elasticemail.com",
-//     Username : "username",
-//     Password : "password",
-//     To : 'them@website.com',
-//     From : "you@isp.com",
-//     Subject : "This is the subject",
-//     Body : "And this is the body"
-// }).then(
-//   message => alert(message)
-// );
+
+
+// const emailConfig = {
+//   host: 'smtp.elasticemail.com',
+//   username: 'your_username',
+//   password: 'your_password',
+//   to: 'them@website.com',
+//   from: 'you@isp.com',
+//   subject: `Figurine Request - ${superheroName}`,
+//   body: `
+//     Unique ID: ${UniqueID}
+//     Figurine: ${superheroName}
+
+//     Thank you for your request!`,
+// };
